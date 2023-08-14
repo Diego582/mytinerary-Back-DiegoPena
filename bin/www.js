959ea00ea13619ca50d1e545e1911ed9fb779c35
Diego <81425000+Diego582@users.noreply.github.com>
2023-08-14 10:14:24 -0300

    Mongo (#1)
    * mongo
    
    * mongo

--- bin/www.js
+++ bin/www.js
@@ -2,12 +2,12 @@ import app from "../app.js";
 import debug from "debug";
 //var http = require('http');
 import http from "http";
-
+import { connect } from "mongoose";
 
 //PORT
 
-let port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
+let port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
 
 /**
  * Create HTTP server.
@@ -19,10 +19,15 @@ let server = http.createServer(app);
  * Listen on provided port, on all network interfaces.
  */
 
-let ready = () => console.log("server ready on port " + port);
-server.listen(port,ready);
-server.on('error', onError);
-server.on('listening', onListening);
+let ready = () => {
+  console.log("server ready on port " + port);
+  connect(process.env.LINK_DB)
+    .then(() => console.log("database connected"))
+    .catch((err) => console.log(err));
+};
+server.listen(port, ready);
+server.on("error", onError);
+server.on("listening", onListening);
 
 /**
  * Normalize a port into a number, string, or false.
@@ -49,22 +54,20 @@ function normalizePort(val) {
  */
 
 function onError(error) {
-  if (error.syscall !== 'listen') {
+  if (error.syscall !== "listen") {
     throw error;
   }
 
-  var bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
+  var bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
 
   // handle specific listen errors with friendly messages
   switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
       process.exit(1);
       break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
       process.exit(1);
       break;
     default:
@@ -78,8 +81,6 @@ function onError(error) {
 
 function onListening() {
   var addr = server.address();
-  var bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  debug('Listening on ' + bind);
+  var bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+  debug("Listening on " + bind);
 }
