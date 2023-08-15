5103441705988dad20ade70a09b8d8530b984f3b
Diego <81425000+Diego582@users.noreply.github.com>
2023-08-15 12:27:05 -0300

    endpoint (#3)

--- controllers/cities/create.js
+++ controllers/cities/create.js
@@ -1,14 +1,21 @@
 import City from "../../models/City.js";
+import User from "../../models/User.js";
 
 export default async (req, res) => {
+  let newCity = req.body;
   try {
-    let newCity = await City.create(req.body);
+    let user = await User.findOne({ mail: newCity.admin_id });
+    let admin_id = await user._id;
+    newCity.admin_id = admin_id;
+    await City.create(newCity);
+
     return res.status(201).json({
       success: true,
       message: "city created",
       response: newCity._id,
     });
   } catch (error) {
+    console.log(error);
     return res.status(400).json({
       success: false,
       message: "not created",
--- routes/cities.js
+++ routes/cities.js
@@ -1,8 +1,15 @@
 import express from "express";
 import read from "../controllers/cities/read.js";
+import create from "../controllers/cities/create.js";
+import readOne from "../controllers/cities/readOne.js";
+import update from "../controllers/cities/update.js";
+import destroy from "../controllers/cities/destroy.js";
 
 const router = express.Router();
 
 router.get("/", read);
-
+router.get("/:id", readOne);
+router.post("/", create);
+router.put("/:id", update);
+router.delete("/:id", destroy);
 export default router;
