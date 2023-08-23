6443c7244965b4132b80dcd3b8fa68b4ce0d1e08
Diego <81425000+Diego582@users.noreply.github.com>
2023-08-23 01:39:39 -0300

    sprint2 (#6)

--- controllers/cities/read.js
+++ controllers/cities/read.js
@@ -2,13 +2,21 @@ import City from "../../models/City.js";
 
 export default async (req, res, next) => {
   try {
-    let cities = await City.find()
+    console.log(req.query.city);
+    let searchObject = {};
+    if (req.query.city) {
+      searchObject.city = new RegExp("^" + req.query.city, "i");
+    }
+    let allCities = await City.find(
+      searchObject,
+      "country fundation photo city admin_id"
+    )
       .select("country city photo smalldescription admin_id")
       .populate("admin_id", "name mail photo -_id");
     return res.status(200).json({
       success: true,
       message: "cities found",
-      response: cities,
+      response: allCities,
     });
   } catch (error) {
     next(error);
