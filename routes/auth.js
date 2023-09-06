3332b5ecb0cb29272644ee7bc10518797c56db4e
Diego <81425000+Diego582@users.noreply.github.com>
2023-09-06 12:35:40 -0300

    register2 (#11)

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,18 @@
 import { Router } from "express";
 import register from "../controllers/auth/register.js";
+import validator from "../middlewares/validator.js";
+import registerSchema from "../schemas/register.js";
+import existUser from "../middlewares/existUser.js";
+import isValidPass from "../middlewares/isValidPass.js";
 
 let auth_router = Router();
 
-auth_router.post("/signup", register);
+auth_router.post(
+  "/signup",
+  validator(registerSchema),
+  existUser,
+  isValidPass,
+  register
+);
 
 export default auth_router;
