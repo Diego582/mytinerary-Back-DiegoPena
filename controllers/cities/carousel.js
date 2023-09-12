d7306cd36f2446705f30352cc8c3ea14b076f812
Diego Peña <penaomardiego@gmail.com>
2023-09-12 00:11:50 -0300

    carouselv2

--- controllers/cities/carousel.js
+++ controllers/cities/carousel.js
@@ -7,11 +7,25 @@ export default async (req, res, next) => {
     let all = await City.find({}, "city photo")
       .sort({ fundation: -1 })
       .limit(limit);
+    let dataCarousel = [];
+    if (all.length > 0) {
+      let arrayCities = [];
+      let count = 0;
+      for (let city of all) {
+        count++;
+        arrayCities.push(city);
+        if (count > 3) {
+          dataCarousel.push(arrayCities);
+          count = 0;
+          arrayCities = [];
+        }
+      }
+    }
+
     return res.status(200).json({
       succes: true,
       message: "cities to show on carousel",
-      data_carousel: all,
-      count,
+      data_carousel: dataCarousel,
     });
   } catch (error) {
     next(error);
