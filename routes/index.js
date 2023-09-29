cc1d83300061d231a323f232d1bad87af24bc181
Diego <penaomardiego@gmail.com>
2023-09-29 09:52:25 -0300

    agregados

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ import cityRouter from "./cities.js";
 import itineraryRouter from "./itineraries.js";
 import authRouter from "./auth.js";
 import activityRouter from "./activities.js";
+import likesRouter from "./likes.js";
 
 const router = express.Router();
 
@@ -12,5 +13,6 @@ router.use("/cities", cityRouter);
 router.use("/itineraries", itineraryRouter);
 router.use("/auth", authRouter);
 router.use("/activities", activityRouter);
+router.use("/likes", likesRouter);
 
 export default router;
